2e88292c44020313e1f747e6cabdd34e6094e558
NicolasUD <jnvmud@gmail.com>
2019-10-21 01:17:26 -0500

    Snackbar y Boton de acción

--- app/src/main/java/com/example/petagram/MainActivity.java
+++ app/src/main/java/com/example/petagram/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.petagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void fabClick(View view) {
+        Snackbar.make(view,getResources().getString(R.string.mensaje),Snackbar.LENGTH_SHORT)
+                .setAction(getResources().getString(R.string.accion), new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.i("El gato es: ","Malvado");
+                    }
+                })
+                .setActionTextColor(getResources().getColor(R.color.colorPrimary))
+                .show();
     }
 }
